81a195f33a8c6084d215cbac2eac55ddb7a3e33e
Abdwahab Adekola <adekolaabdwahababisoye@gmail.com>
2022-07-18 21:01:26 +0100

    prd mgt

--- index.js
+++ index.js
@@ -8,14 +8,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+app.get('/',(req,res)=>{res.send('welcome to the app')})
 app.use("/users", require("./routes/users"));
 app.use("/products", require("./routes/products"));
 app.use("/comments", require("./routes/comments"));
 
-
-
 let port = process.env.PORT || 3000
 app.listen(port, ()=>console.log('listening at port ', port))
-
-
-
